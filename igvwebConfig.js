e966aac7d29a7d7c625b6a7dd06b4ac727381fc0
mayalivshits <163058332+mayalivshits@users.noreply.github.com>
2025-05-03 16:40:35 +0300

    Add files via upload

---
+++ igvwebConfig.js
@@ -0,0 +1,36 @@
+var igvwebConfig = {
+
+    genomes: "resources/genomes.json",
+    trackRegistryFile: "resources/tracks/trackRegistry.json",
+
+    // Supply a drobpox api key to enable the Dropbox file picker in the load menus.  This is optional
+    //dropboxAPIKey: "...",
+
+    // Supply a Google client id to enable the Google file picker in the load menus.  This is optional
+    //clientId: "...",
+    //apiKey: "...",
+
+    // Provide a URL shorterner function or object.   This is optional.  If not supplied
+    // sharable URLs will not be shortened.  If using tinyURL supply an api token
+    // urlShortener: {
+    //     provider: "tinyURL",
+    //     api_token: "<your tinyurl token>"
+    // },
+    // urlShortener: function(longURL) {...   return shortendURL}
+
+    enableCircularView: true,
+
+    restoreLastGenome: true,
+
+    igvConfig:
+        {
+            genome: "hg19",
+            locus: "all",
+            genomeList: "resources/genomes.json",
+            queryParametersSupported: true,
+            showChromosomeWidget: true,
+            showSVGButton: false,
+            tracks: []
+        }
+
+}
